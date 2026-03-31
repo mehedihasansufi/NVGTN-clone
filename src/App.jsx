c055de9d8dcadc35f1b44a9eba44fa4df1689da0
mehedihasansufi <hasanmehedi205324@gmail.com>
2026-03-31 20:06:38 +0600

    NVGTN website first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <>
+   
+   <h1 className='bg-black text-white py-1 flex justify-center px-8'>Free shipping to Bangladesh on all orders above Tk18498</h1>
+   
+   </>
+  )
+}
+
+export default App
